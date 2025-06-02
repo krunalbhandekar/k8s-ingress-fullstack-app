880c885dc07eb3856bff78274bb4b15db16ad17d
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-06-02 13:40:20 +0530

    feat(Dockerfile): Added dockerfile

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 routerInit(app);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, async () => {
   console.log(`Server Running on port ${port}`);
